8887df5c73edf980ac020f3cbbd2cc4be748b6be
Nicklas Wiegandt <nicklas.wiegandt@adesso.de>
2026-01-17 16:45:31 +0100

    refactor: remove unnecessary SearchData wrapper for cleaner data structure

--- src/main/kotlin/eu/wiegandt/zdfmediathekmcp/model/SearchData.kt
+++ src/main/kotlin/eu/wiegandt/zdfmediathekmcp/model/SearchData.kt
@@ -1,9 +1,5 @@
 package eu.wiegandt.zdfmediathekmcp.model
 
-data class SearchData(
-    val searchDocuments: SearchDocumentsResult
-)
-
 data class SearchDocumentsResult(
     val results: List<SearchResultItemWrapper>
 )
